fbec58dea61ed1effb1bed7d9191393aa7ba52d8
Anh Nghia Nguyen <nghianghesi@gmail.com>
2019-09-07 12:59:44 -0500

    rename type, fields, element

--- NetCoreSoap/Services/CalculatorService.cs
+++ NetCoreSoap/Services/CalculatorService.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace CalculatorService
 {
     public class SimpleCalculatorService : ISimpleCalculator
     {
-        public Task<GetDataResponse> Add(int num1, int num2)
+        public Task<AddDataResponse> Add([XmlElement(ElementName = "Request")] AddDataRequest request)
         {
-            return Task.Run(() => new GetDataResponse() { Result = num1 + num2 });
+            return Task.Run(() => new AddDataResponse() { Result = request.num1 + request.num2 });
         }
     }
 }
--- NetCoreSoap/Services/ISimpleCalculator.cs
+++ NetCoreSoap/Services/ISimpleCalculator.cs
@@ -5,21 +5,35 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace CalculatorService
 {
-    [ServiceContract(Namespace = "http://www.test2.org/test2/types")]
+    [ServiceContract( Namespace = "http://www.test2.org/test2/types", Name = "ApuService")]
     public interface ISimpleCalculator
     {
-        [OperationContract()]
-        Task<GetDataResponse> Add(int num1, int num2);
+        [OperationContract(Name = "ApuV3", AsyncPattern = true)]
+        [return:XmlElement(ElementName = "ApuAddV3Response")]
+        Task<AddDataResponse> Add([XmlElement(ElementName = "Request")] AddDataRequest request);
     }
 
 
-    [DataContract(Namespace = "http://www.test2.org/test2/types")]
-    public class GetDataResponse
+    [DataContract(Namespace = "http://www.test2.org/test2/types", Name = "ApuResponse")]
+
+    public class AddDataResponse
     {
         [DataMember(Name = "FinalResult")]
+        [XmlElement(ElementName = "FinalResult")]
         public int Result { get; set; }
+    }
+
+
+    [DataContract(Namespace = "http://www.test2.org/test2/types", Name = "ApuRequest")]
+    public class AddDataRequest
+    {
+        [XmlElement(ElementName = "n1")]
+        public int num1 { get; set; }
+        [XmlElement(ElementName = "n2")]
+        public int num2 { get; set; }
     }
 }
