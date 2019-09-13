694f952d00d37fd6269cc2c1400316f15f6e77f9
Anh Nghia Nguyen <nghianghesi@gmail.com>
2019-09-12 21:52:16 -0500

    use soap action header

--- NetCoreSoap/Services/ISimpleCalculator.cs
+++ NetCoreSoap/Services/ISimpleCalculator.cs
@@ -9,11 +9,11 @@ using System.Xml.Serialization;
 
 namespace CalculatorService
 {
-    [ServiceContract( Namespace = "http://www.test2.org/test2/types", Name = "ApuService")]
+    [ServiceContract( Namespace = "http://www.test2.org/test2/types", Name = "ApuService")]    
     public interface ISimpleCalculator
     {
-        [OperationContract(Name = "ApuAddV3", AsyncPattern = true)]
-        Task<AddDataResponse> Add([XmlElement(ElementName = "RequestParam")] AddDataRequest request);
+        [OperationContract(Name = "ApuAddV3", Action = "ApuAddV3")]
+        Task<AddDataResponse> Add([XmlElement("ApuRequest")] AddDataRequest request);
     }
 
 
@@ -26,10 +26,13 @@ namespace CalculatorService
 
 
     [DataContract(Namespace = "http://www.test2.org/test2/types", Name = "ApuRequest")]
+    [MessageContract()]
     public class AddDataRequest
     {
+        [MessageBodyMember(Name = "n1")]
         [DataMember(Name = "n1")]
         public int num1 { get; set; }
+        [MessageBodyMember(Name = "n2")]
         [DataMember(Name = "n2")]
         public int num2 { get; set; }
     }
